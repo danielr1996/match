1fe116bcd645ed928ba871f547b7b8a720bb4324
Daniel Richter <danielrichter@posteo.de>
2021-03-07 03:36:01 +0100

    Update match.ts

--- match.ts
+++ match.ts
@@ -1,2 +1,3 @@
 // @ts-ignore
 export const match = <R>(tests: [boolean, R][]): R => tests.filter(([test])=>test).map(([_,value])=>value)[0];
+export const zip = <T,U>(a1: T[], a2: U[]): [T,U][] => a1.map((e,i)=>[e,a2[i]]);
