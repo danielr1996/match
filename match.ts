47f421bd27470f6d6ce65c0c57398dfde4476dcf
Daniel Richter <danielrichter@posteo.de>
2021-03-07 03:37:55 +0100

    Update match.ts

--- match.ts
+++ match.ts
@@ -1,3 +1,6 @@
 // @ts-ignore
 export const match = <R>(tests: [boolean, R][]): R => tests.filter(([test])=>test).map(([_,value])=>value)[0];
 export const zip = <T,U>(a1: T[], a2: U[]): [T,U][] => a1.map((e,i)=>[e,a2[i]]);
+export interface Constructable<T> {
+    new(...args: any[]): T;
+}
